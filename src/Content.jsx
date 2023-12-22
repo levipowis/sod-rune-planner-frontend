fc1b6e36734fbfe1af66d8624727a38e51f14765
Levi Powis <lpowis97@gmail.com>
2023-12-21 21:47:47 -0500

    added functionality to hide Login and Signup while logged in and to hide Logout when not

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import { Signup } from "./Signup";
+import { Login } from "./Login";
+
 export function Content() {
   return (
-    <main>
-      <h1>Welcome to React!</h1>
-    </main>
+    <div>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,18 @@
+import { LogoutLink } from "./LogoutLink";
+import { Link } from "react-router-dom";
+
 export function Header() {
   return (
     <header>
-      <nav>
-        <a href="#">Home</a> | <a href="#">Link</a>
-      </nav>
+      <Link to="/">Home |</Link>
+      {localStorage.jwt === undefined ? (
+        <>
+          <Link to="/signup"> Signup |</Link>
+          <Link to="login"> Login</Link>
+        </>
+      ) : (
+        <LogoutLink />
+      )}
     </header>
   );
 }
