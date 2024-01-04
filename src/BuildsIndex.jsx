39e0c5dcaf5df7eb0a383d802089f1a353b32e9e
Levi Powis <lpowis97@gmail.com>
2024-01-03 21:18:53 -0500

    updated styling on BuildsIndex.jsx

--- src/BuildsIndex.jsx
+++ src/BuildsIndex.jsx
@@ -25,8 +25,14 @@ export function BuildsIndex(props) {
           {props.builds.map((build) => (
             <tr key={build.id}>
               <td className="text-warning" scope="row">
-                {build.build_name}
-                <button type="button" className="btn btn-link link-light" onClick={() => props.onShowBuild(build)}>
+                <button
+                  type="button"
+                  className="p-1 btn btn-link link-warning"
+                  onClick={() => props.onShowBuild(build)}
+                >
+                  {build.build_name}
+                </button>
+                <button type="button" className="p-1 btn btn-link link-light" onClick={() => props.onShowBuild(build)}>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
@@ -42,9 +48,21 @@ export function BuildsIndex(props) {
                     />
                   </svg>
                 </button>
+                <button type="button" className="p-1 btn btn-link link-light">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-trash3-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
+                  </svg>
+                </button>
               </td>
-              <td>{build.character_name}</td>
-              <td>{build.character_class}</td>
+              <td scope="row">{build.character_name}</td>
+              <td scope="row">{build.character_class}</td>
             </tr>
           ))}
         </tbody>
