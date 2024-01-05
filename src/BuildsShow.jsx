2f0846cabf8c973a87411a118f5012120704f059
Levi Powis <lpowis97@gmail.com>
2024-01-05 15:51:11 -0500

    added syling to BuildsShow.jsx to use a grid in bootstrap to present rune info in a better way

--- src/BuildsShow.jsx
+++ src/BuildsShow.jsx
@@ -1,12 +1,24 @@
 export function BuildsShow(props) {
   return (
-    <div>
-      <h1>Build information</h1>
-      <p>Build Name: {props.build.build_name}</p>
-      <p>Character Name: {props.build.character_name}</p>
-      <p>Gloves Rune: {props.build.gloves_rune.name}</p>
-      <p>Chest Rune: {props.build.chest_rune.name}</p>
-      <p>Legs Rune: {props.build.legs_rune.name}</p>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div>
+            <h4 className="p-2 text-warning">Gloves Rune</h4>
+          </div>
+          <div className="container p-2 me-4">
+            <div className="border border-secondary rounded bg-black mt-3 p-2">
+              <span className="me-2">
+                <img height="32" width="32" src={props.build.gloves_rune.image_url}></img>
+              </span>
+              <span className="m-2 fs-5">{props.build.gloves_rune.name}</span>
+              <div className="mt-1 text-warning">{props.build.gloves_rune.description}</div>
+            </div>
+          </div>
+        </div>
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+      </div>
     </div>
   );
 }
