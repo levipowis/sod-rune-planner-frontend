b5431184eaf6e88b0c9b98a1c0b35a7c0e3ee927
Levi Powis <lpowis97@gmail.com>
2024-01-10 20:09:16 -0500

    added https to render url in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "sod-rune-planner.onrender.com";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://sod-rune-planner.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
